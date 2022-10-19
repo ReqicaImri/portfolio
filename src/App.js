54a50a59901b3264b8ad5a2f35e7b51afd2b84cf
unknown <reqicaimri@gmail.com>
2022-10-19 12:45:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
